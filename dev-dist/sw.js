aa66aab336e2297f3087d4ae50cfd7a383241613
kang <bigsunms@gmail.com>
2026-03-05 16:40:38 +0900

    update

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "9716aa05042d5c33ebcbd1b9412bea58"
   }, {
     "url": "index.html",
-    "revision": "0.94kskg460oc"
+    "revision": "0.crckn9ulfcs"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
